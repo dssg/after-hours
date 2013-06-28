890fd11533ccf989df9543692e5883e10f45f25f
Jette Henderson <jette.henderson@gmail.com>
2013-06-28 11:22:59 -0500

    changed treatment name generation code into a function that can be imported into Generate_Simulated_Education_Data.R

--- Stats-Code-and-Sim-Data/Code/Generate_Treatment_Center_Names.R
+++ Stats-Code-and-Sim-Data/Code/Generate_Treatment_Center_Names.R
@@ -1,14 +1,19 @@
 # read in the file of adjectives
-sCenterName <- read.table("~/adjectives.txt", header=F, quote="\"")
 
-set.seed(4)
-nPossibleCenters <- 372
-# Construct names for schools (comes from http://www.fanfiction.net/s/7739576/1/106-Stories-of-Us)
-colnames(sCenterName) <- 'Word'
-CenterTypeDraw <- cut(runif(nPossibleCenters), breaks = c(0.0, 0.3, 0.5, 0.7, 0.8, 0.9, 1.0), include.lowest = TRUE)
-sCenterType <- as.character(factor(CenterTypeDraw, labels = c('Camp','Program','Center','Initiative','Club','House')))
-sPossibleCenterName <- paste(sCenterName$Word, sCenterType)
+makeTrtNames <- function(n = nTrt) {
+  sCenterName <- read.table("~/adjectives.txt", header=F, quote="\"")
 
-# Randomly select the number centers you want
-nCenters <- 20
-sCenterName <- sPossibleCenterName[sample(1:nPossibleCenters,nCenters,replace=F)]
+  nPossibleCenters <- 372
+  # Construct names for centers (comes from http://www.fanfiction.net/s/7739576/1/106-Stories-of-Us)
+  colnames(sCenterName) <- 'Word'
+  CenterTypeDraw <- cut(runif(nPossibleCenters), breaks = c(0.0, 0.3, 0.5, 0.7, 0.8, 0.9, 1.0), include.lowest = TRUE)
+  sCenterType <- as.character(factor(CenterTypeDraw, labels = c('Camp','Program','Center','Initiative','Club','House')))
+  sPossibleCenterName <- paste(sCenterName$Word, sCenterType)
+
+  # Randomly select the number centers you want
+  nCenters <- n
+  sCenterName <- sPossibleCenterName[sample(1:nPossibleCenters,nCenters,replace=F)]
+  return(sCenterName)
+}
+
+makeTrtNames(n = 5)
